923384f83f4acc0253dc83090769337edf27f787
totz <yuta.totz@gmail.com>
2014-11-05 20:22:33 +0900

    Setting of error message #15

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, 
+    presence: { message: "* Type the title" },
+    length: { minimum: 3, message: "* Minimum Letter Number is 3" }
 end
