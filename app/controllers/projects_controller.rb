38ddc46d82c5ac8ce6024cc548c626b6a84365a2
totz <yuta.totz@gmail.com>
2014-11-05 20:16:46 +0900

    not null setting #15

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,5 +11,20 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+  
+  def create
+    @project = Project.new(project_params)  # put into the data from form
+    if @project.save
+      redirect_to projects_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def project_params
+    params[:project].permit(:title)
+  end
 
 end
