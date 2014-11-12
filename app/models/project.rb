a9e92c25c16e69c1ac260546504a281d7a787c6a
totz <yuta.totz@gmail.com>
2014-11-12 14:35:22 +0900

    Create model, controller, view for Tasks #20-#21

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
+  has_many :tasks
   validates :title, 
     presence: { message: "* Type the title" },
     length: { minimum: 3, message: "* Minimum Letter Number is 3" }
